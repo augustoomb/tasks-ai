446a032342cecf8cbef63533b2808b57c03f664c
Augusto Barbosa <augustobarbosa1988@gmail.com>
2024-10-25 17:54:22 -0300

    feat: função para criptografar password

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+const bcrypt = require('bcrypt');
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+ export const cryptPass = async (unHashedPass: string) => {
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(unHashedPass, salt);
 }
