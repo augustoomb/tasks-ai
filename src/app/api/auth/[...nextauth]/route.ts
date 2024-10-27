b6c1cdb92e36e2b6925b6ad4f7f63fc733523667
Augusto Barbosa <augustobarbosa1988@gmail.com>
2024-10-27 11:54:31 -0300

    refactor: signIn do Nextaut foi reorganizado, com funções específicas.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,33 +1,39 @@
 import NextAuth from "next-auth/next";
-import CredentialsProvider from "next-auth/providers/credentials";
+import CredentialsProvider from "next-auth/providers/credentials"; 
+import prisma from '@/lib/prisma';
+import { comparePass } from "@/lib/utils";
+
+const findUserByEmail = async (email: string) => {
+  return await prisma.user.findUnique({ where: { email } });
+}
+
+const authorize = async (credentials: any) => {
+  try {
+    const { email, password } = credentials || {};
+    if (!email || !password) return null;
+
+    const userDb = await findUserByEmail(email);
+    if (!userDb) return null;
+    
+    const passIsValid = await comparePass(password, userDb.password);
+
+    return passIsValid ? { id: String(userDb.id), email: userDb.email } : null;
+
+  } catch (error) {
+    console.log("erro: "+error)
+    return null;
+  }
+}
 
 const handler = NextAuth({
-  session: {
-    strategy: "jwt",
-  },
-
-  pages: {
-    signIn: "/login",
-  },  
+  session: { strategy: "jwt" },
+  pages: { signIn: "/login" },  
 
   providers: [
     CredentialsProvider({
       name: "Credentials",
-      credentials: {
-        email: {},
-        password: {},
-      },
-      async authorize(credentials, req) {
-        await new Promise((resolve) => setTimeout(resolve, 500));
-        const user = { id: "1", email: "j@j.com" };
-
-        // console.log("credentials: "+JSON.stringify(credentials))
-        // console.log("req: "+JSON.stringify(req))
-
-        if (user) { return user;  }
-
-        return null;
-      },
+      credentials: { email: {}, password: {} },
+      authorize
     }),
   ],
 });
