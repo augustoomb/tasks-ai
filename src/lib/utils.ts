78f908df5fa07dce29ab470c77d56d57b068caea
Augusto Barbosa <augustobarbosa1988@gmail.com>
2024-10-27 11:56:00 -0300

    feat: função para comparar passwords com bcryptjs

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,7 +6,11 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
- export const cryptPass = async (unHashedPass: string) => {
+export const cryptPass = async (unHashedPass: string) => {
   const salt = await bcrypt.genSalt(10);
   return await bcrypt.hash(unHashedPass, salt);
+}
+
+export const comparePass = async (unHashedPass?: string, hashedPass?: string) => {
+  return await bcrypt.compare(unHashedPass, hashedPass);
 }
