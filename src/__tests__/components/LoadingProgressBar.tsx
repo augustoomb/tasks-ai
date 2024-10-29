592a81cf996856df56341d3c8332f78136505100
Augusto Barbosa <augustobarbosa1988@gmail.com>
2024-10-29 20:13:37 -0300

    test: teste para o componente LoadingProgressBar

---
+++ src/__tests__/components/LoadingProgressBar.tsx
@@ -0,0 +1,22 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import LoadingProgressBar from '@/components/LoadingProgressBar'
+ 
+describe('Para o componente LoadingProgressBar, testa se: ', () => {
+  
+  const LoadingProgressBarProps = {
+    isLoading: true,
+    progress: 30
+  } 
+  
+  beforeEach(() => {
+    render(<LoadingProgressBar {...LoadingProgressBarProps}/>)
+  })
+
+  it('Barra de progresso é renderizada', () => {   
+
+    const loadingBar = screen.getByTestId('loading-progress-bar')
+ 
+    expect(loadingBar).toBeInTheDocument()
+  })
+})
