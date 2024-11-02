54c9c1446fdab2951b631d7e5f71289fb7544a99
Augusto Barbosa <augustobarbosa1988@gmail.com>
2024-11-02 12:00:09 -0300

    feat: initial page. Página para selecionar informações logo após login

--- src/app/initial/page.tsx
+++ src/app/initial/page.tsx
@@ -1,11 +1,32 @@
+import { getServerSession } from "next-auth";
 import WithAssistant from "@/components/initial/WithAssistant";
 import WithoutAssistant from "@/components/initial/WithoutAssistant";
+import { fetchResponse } from "@/lib/utils";
+import { Suspense } from "react";
 
-export default function InitialPage() {
+export default async function InitialPage() {
     return (
         <div className="flex md:w-screen md:h-screen items-center justify-center">
-            
-                
+            <Suspense fallback={<p>Carregando...</p>}> 
+                <GetAssistants />
+            </Suspense>                
         </div>
     )
+}
+
+async function GetAssistants() {
+    const session = await getServerSession();
+    const email = session?.user?.email || "";
+    const response = await fetchResponse(`/user?email=${email}`, "GET");
+
+    const data = await response.json();
+
+    const user: any = data.user;
+
+    if (user.Assistant && user.Assistant.length > 0) {
+        return <WithAssistant assistants={user.Assistant} />
+    } else {
+        return <WithoutAssistant />
+    }
+    
 }
