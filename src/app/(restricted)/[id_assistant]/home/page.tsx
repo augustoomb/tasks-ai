9a6ff71f33e24152f338daefd78c55721d7d4cae
Augusto Barbosa <augustobarbosa1988@gmail.com>
2024-11-04 16:52:06 -0300

    feat: home page agora recebe o id do assistente selecionado

--- src/app/(restricted)/[id_assistant]/home/page.tsx
+++ src/app/(restricted)/[id_assistant]/home/page.tsx
@@ -1,14 +1,12 @@
-import BreadCrumb from "@/components/shared/Breadcrumb"
+import BreadCrumb from "@/components/shared/Breadcrumb";
 
 
 export default function Home({ params }: { params: { id_assistant: string } }) {
 
     const elements = [{name: "Home", href: "/"}]
 
-    console.log("Estou na home... "+params.id_assistant)
-
     return(
-        <>
+        <> 
             <BreadCrumb {...{elements}}/>
 
             <h1>Home</h1>
