2761a29a84d5f47968a5a82ce1188bdf78a812a9
Augusto Barbosa <augustobarbosa1988@gmail.com>
2024-11-05 16:09:02 -0300

    feat: implementando funcionalidade do Google API na home page

--- src/app/(restricted)/[id_assistant]/home/page.tsx
+++ src/app/(restricted)/[id_assistant]/home/page.tsx
@@ -1,9 +1,32 @@
 import BreadCrumb from "@/components/shared/Breadcrumb";
+import { fetchResponse } from "@/lib/utils";
 
 
-export default function Home({ params }: { params: { id_assistant: string } }) {
+export default async function Home({ params }: { params: { id_assistant: string } }) {
+  
+    // if (params.id_assistant) {}
+    
 
-    const elements = [{name: "Home", href: "/"}]
+    // const response = await fetchResponse(`/sheets`, "GET");
+
+    // const data = await response.json();
+
+    // console.log(data.values);
+    // // retorno:
+    // [
+    //     [ 'partida_id', 'rodata', 'clube', 'atleta', 'minuto' ],
+    //     [ '4607', '1', 'Fluminense', 'Nirley da Silva Fonseca', '59' ],
+    //     [ '4607', '1', 'Fluminense', 'Fred', '45' ],
+    //     [ '4607', '1', 'Fluminense', 'Rafael Sóbis', '31' ],
+    //     [ '4608', '1', 'Internacional', 'Charles Aránguiz', '6' ],
+    //     [ '4612', '1', 'Cruzeiro', 'Nílton Ferreira Júnior', '63' ],
+    //     [ '4612', '1', 'Cruzeiro', 'Marcelo Moreno', '90' ],
+    //     [ '4612', '1', 'Bahia', 'Anderson Talisca', '80' ]
+    // ]
+
+
+    
+    const elements = [{name: "Home", href: "/"}];
 
     return(
         <> 
