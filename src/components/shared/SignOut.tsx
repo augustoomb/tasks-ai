578d0b4ed80c95eda4e51476740668eb34535d8e
Augusto Barbosa <augustobarbosa1988@gmail.com>
2024-11-06 17:10:23 -0300

    feat: ao efetuar logout, agora a aplicação esvazia o cookie que contem o id do assistente

--- src/components/shared/SignOut.tsx
+++ src/components/shared/SignOut.tsx
@@ -1,14 +1,19 @@
 "use client";
 
 import { signOut } from "next-auth/react";
-import Link from "next/link";
 import { LogOut } from "lucide-react";
+import { deleteCookie } from "@/lib/cookies";
 
 export default function SignOut() {
     return(
-        <button style={{ width: "100%", display: "flex", alignItems: "center" }} onClick={() => signOut({ callbackUrl: "/login" })}>
+        <button style={{ width: "100%", display: "flex", alignItems: "center" }} onClick={() => onClick()}>
             <LogOut className="mr-2 h-4 w-4" />
             <span>Sair</span>
         </button>
     )    
+}
+
+async function onClick() {
+    await deleteCookie("assistantId")
+    signOut({ callbackUrl: "/login" })
 }
