46efcf6416548475b3e62a4333e778c7ae7ca4d7
Augusto Barbosa <augustobarbosa1988@gmail.com>
2024-11-08 14:16:31 -0300

    feat: ação de logout agora mostra o loading enquanto é executada

--- src/components/shared/SignOut.tsx
+++ src/components/shared/SignOut.tsx
@@ -3,17 +3,27 @@
 import { signOut } from "next-auth/react";
 import { LogOut } from "lucide-react";
 import { deleteCookie } from "@/lib/cookies";
+import { useState } from "react";
 
 export default function SignOut() {
+    const [loading, setLoading] = useState(false);
+
     return(
-        <button style={{ width: "100%", display: "flex", alignItems: "center" }} onClick={() => onClick()}>
-            <LogOut className="mr-2 h-4 w-4" />
-            <span>Sair</span>
+        <button disabled={loading} style={{ width: "100%", display: "flex", alignItems: "center" }} onClick={() => handleClick(setLoading)}>
+            {loading ? "Carregando..." : <><LogOut className="mr-2 h-4 w-4" /> Sair</>}
         </button>
     )    
 }
 
-async function onClick() {
-    await deleteCookie("assistantId")
-    signOut({ callbackUrl: "/login" })
+async function handleClick(setLoading: any) {
+    setLoading(true);
+
+    try {
+        await deleteCookie("assistantId")
+        await signOut({ callbackUrl: "/login" })
+    } catch (error) {
+        console.error("Erro ao efetuar logout:", error);
+    } finally {
+        setLoading(false);
+    }    
 }
