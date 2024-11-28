6d8b058bb92a19bb7d81dcdeb7ef5746e267c136
Augusto Barbosa <augustobarbosa1988@gmail.com>
2024-11-28 14:48:49 -0300

    feat: rota de api para adicionar novo modulo

---
+++ src/app/api/module/route.ts
@@ -0,0 +1,51 @@
+import { ZodIssue } from 'zod';
+import prisma from "@/lib/prisma";
+import { Prisma } from "@prisma/client";
+import { NextResponse } from "next/server";
+import { moduleSchema } from "@/schemas/moduleSchema";
+
+export async function POST(req: Request) {
+    try {
+        const { name, description, status } = await req.json();
+        const validatedModule = moduleSchema.safeParse({ name, description, status });
+
+        if(!validatedModule.success) {
+            return NextResponse.json(
+                {
+                    errors: validatedModule.error.issues.map((issue: ZodIssue) => {
+                        return {
+                            field: issue.path[0],
+                            message: issue.message
+                        }
+                    }),
+                },
+                { status: 400 }
+            );
+        }
+
+        await prisma.module.create({
+            data: {
+                name, description, status,
+                users: {
+                    create: []
+                }
+            }
+        });
+        return NextResponse.json({ success: 'Módulo criado com sucesso' }, { status: 201 });
+    } catch (e) {
+        if (e instanceof Prisma.PrismaClientKnownRequestError) {
+            if (e.code === 'P2002') {
+                return NextResponse.json({
+                    "errors": [
+                        {
+                            "field": "name",
+                            "message": "Nome do módulo já foi utilizado"
+                        }
+                    ]
+                }, { status: 409 });
+            }
+            return NextResponse.json({ error: e }, { status: 500 });
+        }
+        return NextResponse.json({ error: JSON.stringify(e) }, { status: 500 });
+    }
+}
