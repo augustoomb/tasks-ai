83d2971b225b4570e37c86cba7054a0c51022437
Augusto Barbosa <augustobarbosa1988@gmail.com>
2024-12-03 13:47:14 -0300

    feat: rota de api para trazer módulos do banco (filtragem por módulo e usuários específicos)

---
+++ src/app/api/module/[id]/route.ts
@@ -0,0 +1,38 @@
+import { ZodIssue } from 'zod';
+import prisma from "@/lib/prisma";
+import { Prisma } from "@prisma/client";
+import { NextResponse } from "next/server";
+import { moduleSchema } from "@/schemas/moduleSchema";
+
+export async function POST(req: Request) {
+}
+
+export async function GET(req: Request, { params }: { params: { id: string } }) {
+    try {
+        const { id } = params; // id do módulo vem via URL
+
+        const {searchParams} = new URL(req.url);
+        const userId = Number(searchParams.get("userId")) || 0;
+
+
+        const modules = await prisma.module.findUnique({
+            where: {
+                id: Number(id),
+                users: {
+                    some: {
+                        user: {
+                            id: userId
+                        }
+                    }
+                }
+            },
+            include: {
+                users: true
+            }
+        });
+        return NextResponse.json({ modules }, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ error: JSON.stringify(error) }, { status: 500 });
+    }
+}
+
