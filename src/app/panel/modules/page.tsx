7ca652563ab747725aa8d0d3c4ad01d3e87069de
Augusto Barbosa <augustobarbosa1988@gmail.com>
2024-12-03 13:49:15 -0300

    feat: buscando módulos no banco e exibindo na página inicial de módulos

--- src/app/panel/modules/page.tsx
+++ src/app/panel/modules/page.tsx
@@ -1,6 +1,10 @@
 import BreadCrumb from "@/components/shared/Breadcrumb";
 import { fetchResponse } from "@/lib/utils";
-
+import { Module } from "@prisma/client";
+import { Suspense } from "react";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import ItemModule from "@/components/modules/ItemModule";
 
 export default async function Modules() {
       
@@ -10,8 +14,30 @@ export default async function Modules() {
         <> 
             <BreadCrumb {...{elements}}/>
 
-            <h1>Modules</h1>
+            <h1 className="text-3xl font-bold h-16">Modules</h1>
+
+            <Suspense fallback={<p>Carregando...</p>}>
+                <GetModules />
+            </Suspense>
         </>
+    )    
+}
+
+async function GetModules() {
+    const session = await getServerSession(authOptions);
+    const userId = session?.user?.id || "";
+
+    const response = await fetchResponse("/module", "GET");
+    const data = await response.json();
+    const modules: Module[] = data.modules;
+
+    return (
+        modules
+        .filter((module: Module) => module.status !== false)
+        .map((module: Module) => {
+            return (
+                <ItemModule key={module.id} module={module} userId={userId} />
+            )
+        })
     )
-    
 }
