be64fb02dbfc12049ef195ad60f83798a95dd667
Augusto Barbosa <augustobarbosa1988@gmail.com>
2024-12-22 20:14:55 -0300

    feat: rota de api para CHAT, que vai receber as mensagens do usuário e retornar os textos

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,35 @@
+import { testeGetGoogleSheetData } from '@/lib/sheets';
+import { openai } from '@ai-sdk/openai';
+import { streamText, tool } from 'ai';
+// import { testeGetGoogleSheetData } from '../sheets/route';
+import { z } from 'zod';
+
+// Allow streaming responses up to 30 seconds
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+
+  // console.log('Mensagens recebidas:', messages);
+
+  const result = streamText({
+    model: openai('gpt-4o-mini'),
+    messages,
+    maxSteps: 3,
+    system: `Você é um assistente útil. Verifique sua base de conhecimento antes de responder a quaisquer perguntas.
+`,
+    
+    tools: {
+      getInformation: tool({
+        description: 'o usuário informará uma célula de planilha (ex: A4) e você fornecerá o conteúdo dessa célula',
+        parameters: z.object({
+          requestedCell: z.string().describe('Celula inicial'),
+        }),
+        execute: async ({ requestedCell }) => testeGetGoogleSheetData(requestedCell)
+      }),
+    },
+  });
+
+  return result.toDataStreamResponse();
+}
+
+// Qual o menor valor da planilha, levando em conta as células A2 até A6
